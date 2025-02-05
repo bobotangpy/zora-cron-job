a1c9e1fba9751902855daaafb2ae682b99778105
Bobo Tang <52185352+bobotangpy@users.noreply.github.com>
2025-02-05 16:37:13 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.wait import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 # Set up Chrome options
